bc926c8c5431699201e34c6133208b44d1549ec5
Kyle Hasan <kylehasan1@gmail.com>
2024-11-09 13:14:21 -0700

    Added user routes

--- backend/routes/user_routes.py
+++ backend/routes/user_routes.py
@@ -1,14 +1,48 @@
 from flask import Blueprint, request, jsonify
 from backend.models import User
 from backend.db import db
-
+from werkzeug.security import generate_password_hash, check_password_hash
 user_blueprint = Blueprint('user_blueprint', __name__)
 
-@user_blueprint.route('/', methods=['POST'])
-def create_user():
+@user_blueprint.route('/signup', methods=['POST'])
+def signup():
     data = request.json
-    new_user = User(username=data['username'], password=data['password'])
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    new_user = User(username=data['username'], password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
-    return jsonify({'message': 'User created'}), 201
+    return jsonify({'message': 'User signed up successfully'}), 201
 
+@user_blueprint.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    user = User.query.filter_by(username=data['username']).first()
+    if user and check_password_hash(user.password, data['password']):
+        return jsonify({'message': 'Login successful'}), 200
+    return jsonify({'message': 'Invalid username or password'}), 401
+
+@user_blueprint.route('/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return jsonify({'id': user.id, 'username': user.username}), 200
+
+@user_blueprint.route('/', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    return jsonify([{'id': user.id, 'username': user.username} for user in users]), 200
+
+@user_blueprint.route('/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    user = User.query.get_or_404(user_id)
+    data = request.json
+    user.username = data.get('username', user.username)
+    user.password = data.get('password', user.password)
+    db.session.commit()
+    return jsonify({'message': 'User updated'}), 200
+
+@user_blueprint.route('/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message': 'User deleted'}), 200

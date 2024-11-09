c814cafbee265a623906e8dcd5dc6e634db534a4
Kyle Hasan <kylehasan1@gmail.com>
2024-11-09 14:14:46 -0700

    added mysql

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost:5432/empathy-connect'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/empathy-connect'
     SQLALCHEMY_TRACK_MODIFICATIONS = False

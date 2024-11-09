fe48eb2e5056ff8d48fc6bbf19ff6924209235f2
Kyle Hasan <kylehasan1@gmail.com>
2024-11-09 12:04:40 -0700

    Structure backend and set up config

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
-app = Flask(__name__)
+db = SQLAlchemy()
 
-@app.route('/')
-def home():
-    return "Hello, Flask!"
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    with app.app_context():
+        from .routes import register_routes
+        register_routes(app)
+        db.create_all()  
+
+    return app

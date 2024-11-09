669e055498ff6a14a4993448a77d8baaca6d309b
promatty <matthewlblue@gmail.com>
2024-11-09 13:16:36 -0700

    ./community/1 to see community

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css'; // Import Tailwind CSS
 import Home from './components/Home';
@@ -7,6 +7,7 @@ import Community from './components/Community';
 import Post from './components/Post';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,11 +16,14 @@ root.render(
       <div className="flex flex-col min-h-screen">
         <Header />
         <div className="flex flex-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/community/:id" element={<Community />} />
-            <Route path="/post/:id" element={<Post />} />
-          </Routes>
+          <Sidebar />
+          <div className="flex-1">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/community/:id" element={<Community />} />
+              <Route path="/post/:id" element={<Post />} />
+            </Routes>
+          </div>
         </div>
         <Footer />
       </div>

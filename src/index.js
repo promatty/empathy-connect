d0e39a097be3ff1c998d19744c9efffe04387d73
Austin Kim <austinkimkimkim@gmail.com>
2024-11-09 16:42:58 -0700

    tweak

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import './index.css'; // Import Tailwind CSS
 import Home from './components/Home';
 import Community from './components/Community';
@@ -8,27 +8,39 @@ import Post from './components/Post';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Sidebar from './components/Sidebar';
-import Profile from './components/Profile';
+import Login from './components/Login';
+import Profile from './components/Profile'
+
+function App() {
+  const location = useLocation();
+
+  const isLoginPage = location.pathname === '/login';
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      {!isLoginPage && <Header />} 
+      <div className="flex flex-1">
+        {!isLoginPage && <Sidebar />} 
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/community/:id" element={<Community />} />
+            <Route path="/post/:id" element={<Post />} />
+            <Route path='/profile' element={<Profile/>} />
+            <Route path="/login" element={<Login />} /> 
+          </Routes>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <div className="flex flex-1">
-          <Sidebar />
-          <div className="flex-1">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/community/:id" element={<Community />} />
-              <Route path="/post/:id" element={<Post />} />
-              <Route path='/profile' element={<Profile/>} />
-            </Routes>
-          </div>
-        </div>
-        <Footer />
-      </div>
+      <App /> 
     </Router>
   </React.StrictMode>
 );

e849db570d933d5bdb5c0628aff2781332437a4c
Austin Kim <austinkimkimkim@gmail.com>
2024-11-09 14:00:45 -0700

    yes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,7 +2,7 @@
 export default function Post({title, body}){
     return (
         <div className="bg-[#E9EDC9] rounded-md p-4 mb-2">
-            <div className="bg-[#FEFAE0] rounded-md border-black border-2 p-2 shadow-2xl">
+            <div className="bg-[#FEFAE0] rounded-md border-black border-2 p-2 shadow-xl">
                 <div className="flex items-center space-x-4 mb-4">
                     <div>
                     <h2 className="font-semibold text-xl text-black">{title}</h2>

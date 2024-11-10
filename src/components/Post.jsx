d61301583619db746e6a2ccb1fcb89378cc8e4ca
Austin Kim <austinkimkimkim@gmail.com>
2024-11-09 20:43:54 -0700

    yur

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,6 @@
 export default function Post({ title, body, username }) {
     return (
-        <div className="bg-[#E9EDC9] rounded-md p-4 mb-2">
+        <div className="bg-[#E9EDC9] hover:bg-[#d3e2a4] rounded-md p-4 mb-2 transition-colors duration-200">
             <div className="bg-[#FEFAE0] rounded-md border-black border-2 p-2 shadow-xl">
                 <div className="flex items-center space-x-4 mb-4">
                     <div>
@@ -8,12 +8,12 @@ export default function Post({ title, body, username }) {
                         <p className="text-sm text-gray-600">Posted by {username}</p>
                     </div>
                     <div>
-                    <button
-                    type="button"
-                    className="w-full py-2 px-4 bg-green-200 text-green-800 font-semibold rounded-md shadow-md hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-2 transition ease-in-out duration-150"
-                    >
-                    Edit
-                    </button>
+                        <button
+                            type="button"
+                            className="w-full py-2 px-4 bg-green-200 text-green-800 font-semibold rounded-md shadow-md hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-2 transition ease-in-out duration-150"
+                        >
+                            Edit
+                        </button>
                     </div>
                 </div>
                 <div className="text-black">
